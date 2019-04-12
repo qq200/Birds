7290c4c4e0376c0c64834838c41a5f27517147ad
PHP CURS <43753261+qq200@users.noreply.github.com>
2019-04-12 20:03:34 +0300

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,13 @@
+<?php
+//user data
+$avatar="https://pickaface.net/gallery/avatar/Garret22785730d3a8d5525.png";
+$nickname="John";
+$birth_year=1990;
+$job="Segnior Developer";
+$phone="+250111111";
+$income=200000; //USD/year
+$experience=10;
+?>
+<hr>
+<img src="<?php print $avatar ?>"/>
+
