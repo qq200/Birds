a65c4eeb750f183aa61081ef68b03293b3b8230e
PHP CURS <43753261+qq200@users.noreply.github.com>
2019-04-13 13:33:29 +0300

    Add files via upload

--- profile.php
+++ profile.php
@@ -8,6 +8,18 @@ $phone="+250111111";
 $income=200000; //USD/year
 $experience=10;
 ?>
+<pre style="padding:60px; border: 2px solid black;">
 <hr>
-<img src="<?php print $avatar ?>"/>
-
+<img src="<?php print $avatar ?>" style="float:right"/>
+<br>
+<h3><?php print $nickname." - ".$birth_year ?></h3>
+<?php print $job ?>
+<br>
+&#9742; : <a href="" type="tel"><?php print $phone ?></a>
+<br>
+<hr>
+Profesional experience: <b> <?php print $experience ?> year</b>
+<br>
+Income: <b> <?php print $income ?> USD/Year </b>
+<hr>
+</pre>
