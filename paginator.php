d94647cc3abd92bb3d1ab017f0e2d78d86a37195
PHP CURS <43753261+qq200@users.noreply.github.com>
2019-04-19 16:10:46 +0300

    Add files via upload

--- paginator.php
+++ paginator.php
@@ -1,24 +1,21 @@
 <?php
 $page=1;
-$page_selected=3;
-
-while($page<=9){
-    if($page==1){
-        print "<";
-    } 
+$page_selected=200;
+while($page<=1000){
+    if($page==1 && $page!==$page_selected){
+        print "<...";
+    }     
     if($page==$page_selected){
         print "[".$page."]";
-    }else{print $page." ";}
-    if($page==9){
-        print ">";
+    }else if($page==$page_selected-5 ||$page==$page_selected-4 || 
+            $page==$page_selected-3 ||$page==$page_selected-2 ||
+            $page==$page_selected-1 ||$page==$page_selected+5 ||
+            $page==$page_selected+4 || $page==$page_selected+3||
+            $page==$page_selected+2 ||$page==$page_selected+1){
+        print $page." ";}
+    if($page==1000 && $page!=+$page_selected){
+        print "...>";
     }
     $page++;
 }
-// daca e activ 1 strelca se pierde, daca 9 ultima se pierde
-
-//1000pages, 2000 activ, 11 show (+-5)
-// <....195...[200]..205..>
-
-
-
 ?>
