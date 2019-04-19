1170fa1ac3105fa79bf98e33d0bb04a18324e19d
PHP CURS <43753261+qq200@users.noreply.github.com>
2019-04-19 19:27:56 +0300

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,25 @@
+<?php
+// switch(expresion){  ===
+    // case value1: code1; break;
+    // case value2: code1; break;
+    // default: code_default; break;
+// }
+// $day=5;
+for($day=1; $day<=7; $day++){
+switch($day){ 
+    case 1: print "Mo"; break;
+    case 2: print "Tu"; break;
+    case 3: print "Wd"; break;
+    case 4: print "Th"; break;
+    case 5: print "Fr"; break;
+    case 6: print "Sa"; break;
+    case 7: print "Su"; break;
+
+    default: print "no such day"; break;
+}
+print "<br>";
+}
+
+
+//do while pe acasa
+?>
