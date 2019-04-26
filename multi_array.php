9730fad9160905f18680bb686a69fec1b04ab66c
PHP CURS <43753261+qq200@users.noreply.github.com>
2019-04-26 19:56:11 +0300

    Add files via upload

---
+++ multi_array.php
@@ -0,0 +1,43 @@
+<?php
+$table=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+print $table[1][2];
+print"<hr>";
+print $table[0][2]+$table[2][0];
+print"<hr>";
+$table[1]=array_reverse($table[1]);
+var_dump($table);
+print"<hr>";
+
+$arrow=[
+    '##****',
+    '**##**',
+    '****##',
+    '**##**',
+    '##****'
+];
+
+foreach($arrow as $value){
+    print $value."<br>";
+}
+//!!!!!acasa: de facut ca sageata sa arate invers
+print"<hr>";
+
+$templates=[
+    "Developer experience is: {X}",
+    "Language: {X}",
+    "{X} is very experienced",
+    "Our company develops for {X} years"
+];
+$value=[
+    5,
+    "English",
+    "Ivan Pupkin",
+    15
+];
+
+?>
