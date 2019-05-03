674441c0fe28c1357527596062affbd31591fd14
PHP CURS <43753261+qq200@users.noreply.github.com>
2019-05-03 18:47:02 +0300

    Add files via upload

---
+++ globals-array.php
@@ -0,0 +1,11 @@
+<?php
+// $GLOBALS-toate variabilele ajung aici
+// $_SERVER- date despre server si unele date a clientului
+// $_GET- date transmise prin metoda GET (forms, a, AJAX)
+// $_POST- -//-post (forms, a,AJAX)
+// $_REQUEST- toate datele care se transmit
+// $_FILES- informatia despre imaginile care se incarca
+// $_COOKIE- inregistrarile clientului, identificarea lui (cartea de vizita)
+// $_SESSION- identificarea clientului
+// $_ENV- enviroment server
+?>
